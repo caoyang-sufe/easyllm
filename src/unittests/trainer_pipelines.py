81782b2341104e691bf0e1fc899a6bd7166ee544
caoyang-sufe <caoyang@163.sufe.edu.cn>
2025-06-16 09:00:17 +0800

    modify run

--- src/unittests/trainer_pipelines.py
+++ src/unittests/trainer_pipelines.py
@@ -55,6 +55,7 @@ def sft_pipeline_test():
 		"trust_remote_code": True,
 		"dataset_train_split": "train[:500]",
 		"dataset_test_split": "train[500:600]",
+		# "lr_scheduler_type": "cosine",	# Default "linear"
 		"use_peft": True,
 		"report_to": "none",
 		"lora_target_modules": ["q_proj", "k_proj", "v_proj"]
@@ -65,38 +66,59 @@ def sft_pipeline_test():
 
 def ppo_pipeline_test():
 	logging.info("PPO unittest ...")
-	model_name_or_path = os.path.join(model_home, model_names[1])
-	"""
-	EleutherAI/pythia-1b-deduped
-	GPTNeoXForCausalLM(
-	  (gpt_neox): GPTNeoXModel(
-		(embed_in): Embedding(50304, 2048)
-		(emb_dropout): Dropout(p=0.0, inplace=False)
-		(layers): ModuleList(
-		  (0-15): 16 x GPTNeoXLayer(
-			(input_layernorm): LayerNorm((2048,), eps=1e-05, elementwise_affine=True)
-			(post_attention_layernorm): LayerNorm((2048,), eps=1e-05, elementwise_affine=True)
-			(post_attention_dropout): Dropout(p=0.0, inplace=False)
-			(post_mlp_dropout): Dropout(p=0.0, inplace=False)
-			(attention): GPTNeoXAttention(
-			  (query_key_value): Linear(in_features=2048, out_features=6144, bias=True)
-			  (dense): Linear(in_features=2048, out_features=2048, bias=True)
-			)
-			(mlp): GPTNeoXMLP(
-			  (dense_h_to_4h): Linear(in_features=2048, out_features=8192, bias=True)
-			  (dense_4h_to_h): Linear(in_features=8192, out_features=2048, bias=True)
-			  (act): GELUActivation()
-			)
-		  )
-		)
-		(final_layer_norm): LayerNorm((2048,), eps=1e-05, elementwise_affine=True)
-		(rotary_emb): GPTNeoXRotaryEmbedding()
-	  )
-	  (embed_out): Linear(in_features=2048, out_features=50304, bias=False)
-	)
-	"""
+	# model_name_or_path = os.path.join(model_home, model_names[1])
+	# """
+	# EleutherAI/pythia-1b-deduped
+	# GPTNeoXForCausalLM(
+	  # (gpt_neox): GPTNeoXModel(
+		# (embed_in): Embedding(50304, 2048)
+		# (emb_dropout): Dropout(p=0.0, inplace=False)
+		# (layers): ModuleList(
+		  # (0-15): 16 x GPTNeoXLayer(
+			# (input_layernorm): LayerNorm((2048,), eps=1e-05, elementwise_affine=True)
+			# (post_attention_layernorm): LayerNorm((2048,), eps=1e-05, elementwise_affine=True)
+			# (post_attention_dropout): Dropout(p=0.0, inplace=False)
+			# (post_mlp_dropout): Dropout(p=0.0, inplace=False)
+			# (attention): GPTNeoXAttention(
+			  # (query_key_value): Linear(in_features=2048, out_features=6144, bias=True)
+			  # (dense): Linear(in_features=2048, out_features=2048, bias=True)
+			# )
+			# (mlp): GPTNeoXMLP(
+			  # (dense_h_to_4h): Linear(in_features=2048, out_features=8192, bias=True)
+			  # (dense_4h_to_h): Linear(in_features=8192, out_features=2048, bias=True)
+			  # (act): GELUActivation()
+			# )
+		  # )
+		# )
+		# (final_layer_norm): LayerNorm((2048,), eps=1e-05, elementwise_affine=True)
+		# (rotary_emb): GPTNeoXRotaryEmbedding()
+	  # )
+	  # (embed_out): Linear(in_features=2048, out_features=50304, bias=False)
+	# )
+	# """
+	# dataset_name = os.path.join(dataset_home, dataset_names[0])
+	# reward_model_path = os.path.join(model_home, model_names[2])
+	# data_processor = None
+	# config_kwargs = {
+		# "output_dir": f"./temp/ppo+{model_name_or_path.split('/')[-1]}+{dataset_name.split('/')[-1]}",
+		# "model_name_or_path": model_name_or_path,
+		# "dataset_name": dataset_name,
+		# "reward_model_path": reward_model_path,
+		# "trust_remote_code": True,
+		# "dataset_train_split": "train[:500]",
+		# "dataset_test_split": "validation[:100]",
+		# # "lr_scheduler_type": "cosine",	# Default "linear"
+		# "use_peft": True,
+		# "report_to": "none",
+		# "lora_target_modules": ["query_key_value"],
+	# }
+	# trainer_kwargs = {
+	# }
+	# ppo_pipeline(data_processor, config_kwargs, trainer_kwargs)
+
+	model_name_or_path = os.path.join(model_home, model_names[0])
 	dataset_name = os.path.join(dataset_home, dataset_names[0])
-	reward_model_path = os.path.join(model_home, model_names[2])
+	reward_model_path = os.path.join(model_home, model_names[3])
 	data_processor = None
 	config_kwargs = {
 		"output_dir": f"./temp/ppo+{model_name_or_path.split('/')[-1]}+{dataset_name.split('/')[-1]}",
@@ -106,13 +128,15 @@ def ppo_pipeline_test():
 		"trust_remote_code": True,
 		"dataset_train_split": "train[:500]",
 		"dataset_test_split": "validation[:100]",
+		# "lr_scheduler_type": "cosine",	# Default "linear"
 		"use_peft": True,
 		"report_to": "none",
-		"lora_target_modules": ["query_key_value"],
+		"lora_target_modules": ["q_proj", "k_proj", "v_proj"],
 	}
 	trainer_kwargs = {
 	}
 	ppo_pipeline(data_processor, config_kwargs, trainer_kwargs)
+
 
 def dpo_pipeline_test():
 	logging.info("DPO unittest ...")
@@ -126,6 +150,7 @@ def dpo_pipeline_test():
 		"trust_remote_code": True,
 		"dataset_train_split": "descriptiveness[:500]",
 		"dataset_test_split": "descriptiveness[500:600]",
+		# "lr_scheduler_type": "cosine",	# Default "linear"
 		"use_peft": True,
 		"report_to": "none",
 		"lora_target_modules": ["q_proj", "k_proj", "v_proj"]
@@ -148,6 +173,7 @@ def grpo_pipeline_test():
 		"trust_remote_code": True,
 		"dataset_train_split": "train[:500]",
 		"dataset_test_split": "validation[:100]",
+		# "lr_scheduler_type": "cosine",	# Default "linear"
 		"use_peft": True,
 		"report_to": "none",
 		"lora_target_modules": ["q_proj", "k_proj", "v_proj"]
@@ -156,26 +182,3 @@ def grpo_pipeline_test():
 		"reward_funcs": reward_funcs,
 	}
 	grpo_pipeline(data_processor, config_kwargs, trainer_kwargs)
-
-
-def ppo_pipeline_test():
-	logging.info("PPO unittest ...")
-	model_name_or_path = os.path.join(model_home, model_names[0])
-	dataset_name = os.path.join(dataset_home, dataset_names[0])
-	reward_model_path = os.path.join(model_home, model_names[3])
-	data_processor = None
-	config_kwargs = {
-		"output_dir": f"./temp/ppo+{model_name_or_path.split('/')[-1]}+{dataset_name.split('/')[-1]}",
-		"model_name_or_path": model_name_or_path,
-		"dataset_name": dataset_name,
-		"reward_model_path": reward_model_path,
-		"trust_remote_code": True,
-		"dataset_train_split": "train[:500]",
-		"dataset_test_split": "validation[:100]",
-		"use_peft": True,
-		"report_to": "none",
-		"lora_target_modules": ["q_proj", "k_proj", "v_proj"],
-	}
-	trainer_kwargs = {
-	}
-	ppo_pipeline(data_processor, config_kwargs, trainer_kwargs)
