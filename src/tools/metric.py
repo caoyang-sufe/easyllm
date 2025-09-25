0c4ad65ac6914d9363efeb80ac5dd9845b131360
caoyang <caoyang@163.sufe.edu.cn>
2025-09-25 23:17:40 +0800

    add more metrics

--- src/tools/metric.py
+++ src/tools/metric.py
@@ -30,11 +30,11 @@ def calc_perplexity(prompt, completion, model):
 # @param predict: [List[<token>]] <token> is Int or Str (prefer Int)
 # @return: [Float]
 def calc_token_accuracy(predict, target):
-	total = min(len(predict_tokens), len(target_tokens))
+	total = min(len(predict), len(target))
 	if total == 0:
 		return 0.
 	correct = sum(
-		1 for predict_token, target_token in zip(predict_tokens, target_tokens)
+		1 for predict_token, target_token in zip(predict, target)
 		if predict_token == target_token
 	)
 	return correct / total
@@ -48,7 +48,7 @@ def calc_bleu(predict, target, min_grams = 1, max_grams = 3):
 	predict_length, target_length = len(predict), len(target)
 	brevity_penalty = 1 if predict_length > target_length else math.exp(1 - predict_length / target_length)
 	def _n_grams(_token_ids, _n):
-		return list() if len(_token_ids) < _n else [_token_ids[_i: _i + _n] for _i in range(len(_token_ids) - n + 1)]
+		return list() if len(_token_ids) < _n else [tuple(_token_ids[_i: _i + _n]) for _i in range(len(_token_ids) - n + 1)]
 	precisions_by_n_grams = list()
 	for n in range(min_grams, max_grams + 1):
 		predict_n_grams = _n_grams(predict, n)
@@ -56,7 +56,7 @@ def calc_bleu(predict, target, min_grams = 1, max_grams = 3):
 		if predict_n_grams and target_n_grams:
 			predict_n_grams_counter = Counter(predict_n_grams)
 			target_n_grams_counter = Counter(target_n_grams)
-			total = predict_length
+			total = len(predict_n_grams)
 			correct = sum(min(predict_n_grams_counter[gram], target_n_grams_counter[gram]) 
 				for gram in predict_n_grams_counter)
 			precision = correct / total
@@ -72,67 +72,69 @@ def calc_bleu(predict, target, min_grams = 1, max_grams = 3):
 # @param target: [List[<token>]]
 # @param predict: [List[<token>]]
 # @param n: [Int] N-grams to compute
-def calc_rouge_n(predict, target, n = 3):
+# @param beta: [Float] F1 weight
+# @return: Dict["prediction": Float, "recall": Float, "f1_score": Float]
+def calc_rouge_n(predict, target, n = 3, beta = 1):
 	predict_length, target_length = len(predict), len(target)
 	def _n_grams(_token_ids, _n):
-		return list() if len(_token_ids) < _n else [_token_ids[_i: _i + _n] for _i in range(len(_token_ids) - n + 1)]
+		return list() if len(_token_ids) < _n else [tuple(_token_ids[_i: _i + _n]) for _i in range(len(_token_ids) - n + 1)]
 	predict_n_grams = _n_grams(predict, n)
-	target_n_grams = _n_grams(target, n)	
+	target_n_grams = _n_grams(target, n)
 	if predict_n_grams and target_n_grams:
 		predict_n_grams_counter = Counter(predict_n_grams)
-		target_n_grams_counter = Counter(target_n_grams)	
-		total_p = predict_length	
-		total_r = target_length
+		target_n_grams_counter = Counter(target_n_grams)
+		total_p = len(predict_n_grams)
+		total_r = len(target_n_grams)
 		correct = sum(min(predict_n_grams_counter[gram], target_n_grams_counter[gram]) 
 			for gram in predict_n_grams_counter)
 		precision = correct / total_p
 		recall = correct / total_r
-		f1_score = 2 * precision * recall / (precision + recall)
+		f1_score = (1 + beta ** 2) * precision * recall / ((beta ** 2) * precision + recall)
 	else:
 		precision = 0
 		recall = 0
 		f_1 = 0
 	return {"precision": precision, "recall": recall, "f1_score": f1_score}
 
-# Calculate ROUGE-W (Weighted ROUGE-L)	
+# Calculate ROUGE-W (Weighted ROUGE-L): https://aclanthology.org/W04-1013.pdf
 # @param target: [List[<token>]]
 # @param predict: [List[<token>]]
 # @param weight_function: [Function] 
 #   - Usually we can use `lambda _x: _x ** 2` or `lambda _x: _x * lambda`
-#   - Default function refers to ROUGE-L, i.e. all weights equal to 1
-def calc_rouge_w(predict, target, weight_function = lambda _x: 1):
+#   - Default function refers to ROUGE-L, see https://aclanthology.org/W04-1013.pdf for more details
+# @param weight_function_reverse: [Function] the reverse function of weight_function
+# @param beta: [Float] F1 weight
+# @return: Dict["prediction": Float, "recall": Float, "f1_score": Float]
+def calc_rouge_w(predict, 
+				 target, 
+				 weight_function = lambda _x: _x,
+				 weight_function_reverse = lambda _x: _x,
+				 beta = 1
+				 ):
 	predict_length, target_length = len(predict), len(target)
 	if predict_length and target_length:
-		# @param _s_1: [List[Int]] Sequence 1
-		# @param _s_2: [List[Int]] Sequence 2
-		# @param _w_f: [List[Int]] Weight function
-		def _easy_lcs(_s_1, _s_2, _w_f):
-			_l_1, _l_2 = len(_s_1), len(_s_2)
-			_dp = [[0] * (_l_2 + 1) for _ in range(_l_1 + 1)]
-			_w = [[0] * (_l_2 + 1) for _ in range(_l_1 + 1)]
+		# @param _x: [List[Int]] Sequence 1
+		# @param _y: [List[Int]] Sequence 2
+		# @param _f: [List[Int]] Weight function
+		def _weighted_lcs(_x, _y, _f):
+			_m, _n = len(_x), len(_y)
+			_c = [[0] * (_n + 1) for _ in range(_m + 1)]
+			_w = [[0] * (_n + 1) for _ in range(_m + 1)]
 			# Weighted LCS
-			for i in range(_l_1):
-				for j in range(_l_2):
-					if _s_1[i] == _s_2[j]:
-						_w_update = _w[i][j] + 1
-						_w[i + 1][j + 1] = _w_update
-						_dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j], dp[i][j] + _weight)
+			for _i in range(_m):
+				for _j in range(_n):
+					if _x[_i] == _y[_j]:
+						_k = _w[_i][_j]
+						_c[_i + 1][_j + 1] = _c[_i][_j] + _f(_k + 1) - _f(_k)
+						_w[_i + 1][_j + 1] = _k + 1
 					else:
-						_w[i + 1][j + 1] = 0
-						_dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
-			# Normal LCS
-			# for i in range(_l_1):
-				# for j in range(_l_2):
-					# if _s_1[i] == _s_2[j]:
-						# _dp[i + 1][j + 1] = _dp[i][j] + 1
-					# else:
-						# _dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
-			return _dp[m][n]
-		scores = []
-		lcs = _easy_lcs(predict, target)
-		precision = lcs / predict_length
-		recall = lcs / target_length
-		f1_score = 2 * precision * recall / (precision + recall)
+						_c[_i + 1][_j + 1] = max(_c[_i][_j + 1], _c[_i + 1][_j])
+						_w[_i + 1][_j + 1] = 0
+			return _c[_m][_n]
+		weighted_lcs = _weighted_lcs(predict, target, weight_function)
+		precision = weight_function_reverse(weighted_lcs / weight_function(predict_length))
+		recall = weight_function_reverse(weighted_lcs / weight_function(target_length))
+		f1_score = (1 + beta ** 2) * precision * recall / ((beta ** 2) * precision + recall)
 	else:
 		precision = 0
 		recall = 0
